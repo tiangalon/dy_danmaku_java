572ab05a40db8140dc825778d78d0086dca5fb9f
tiangalon <272426129@qq.com>
2024-12-22 19:05:02 +0800

    2024/12/22 添加重试功能，可设定0~无限次重试次数和重试间隔时间；关闭cookie警告

--- src/main/java/com/DyDanmaku/DyDanmakuRequest.java
+++ src/main/java/com/DyDanmaku/DyDanmakuRequest.java
@@ -1,6 +1,8 @@
 package com.DyDanmaku;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -13,7 +15,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class myRequest {
+public class DyDanmakuRequest {
+
+     RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
 
      public static String User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
 
@@ -28,19 +32,27 @@ public class myRequest {
         String ttwid = null;
         String roomId = null;
         String user_unique_id = null;
+        String live_status = null;
+        String live_title = null;
+        String nickname = null;
         Map<String, String> params = new HashMap<String, String>();
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
+
+
         try {
             HttpGet httpGet = new HttpGet(url);
+            RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
             httpGet.setHeader("User-Agent", User_Agent);
             httpGet.setHeader("cookie", "__ac_nonce=0" + GenerateToken(20)+ ";/=" +  "live.douyin.com");
+            httpGet.setConfig(defaultConfig);
             CloseableHttpResponse response = httpClient.execute(httpGet);
 
 
             if(response != null){
                 HttpEntity entity = response.getEntity();   // 获取网页内容
                 String result = EntityUtils.toString(entity, "UTF-8");
+
                 /*
                 *正则表达式匹配，实测用时过长，弃用
                 String pattern = ".*((?<=roomId\\\":\\\")\\d+).*";
@@ -51,10 +63,20 @@ public class myRequest {
                     System.out.println("Found roomId: " + m.group());
                 }
                 */
+
                 roomId = result.substring(result.lastIndexOf("roomId")+11, result.lastIndexOf("roomId") + 30);
                 user_unique_id = result.substring(result.indexOf("\\\"user_unique_id\\\":\\\"")+21, result.indexOf("\\\"user_unique_id\\\":\\\"") + 40);
+                live_status = result.substring(result.indexOf("\\\"status_str\\\":")+17, result.indexOf("\\\"status_str\\\":") + 18);
+                String temp = result.substring(result.indexOf("\\\"status_str\\\":")+21);
+                live_title = temp.substring(temp.indexOf("\\\"title\\\":\\\"")+12, temp.indexOf("\\\"title\\\":\\\"") + 100);
+                live_title = live_title.substring(0, live_title.indexOf("\\"));
+                nickname = temp.substring(temp.indexOf("\\\"nickname\\\":\\\"")+15, temp.indexOf("\\\"nickname\\\":\\\"") + 100);
+                nickname = nickname.substring(0, nickname.indexOf("\\"));
                 params.put("roomId", roomId);
                 params.put("user_unique_id", user_unique_id);
+                params.put("live_status", live_status);
+                params.put("live_title", live_title);
+                params.put("nickname", nickname);
 
 
                 Header responseHeader = response.getFirstHeader("Set-Cookie");
